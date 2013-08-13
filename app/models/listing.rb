4a67e5af6bfebe478864884a1e813d2545c8abe4
Tommy Duek <taduek@gmail.com>
2013-08-13 14:35:55 -0400

    Finish distance calculation, search, and display.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,32 +12,25 @@ class Listing < ActiveRecord::Base
   has_many :main_pics, class_name: "Pic",
                        conditions: "pics.thumb_for IS NULL"
 
-     
-#   Listing.select('listings.*, acos(cos(radians(25.95))*cos(radians(-80.14))*cos(radians("zips"."lat"))*cos(radians("zips"."long")) + cos(radians(25.95))*sin(radians(-80.14))*cos(radians("zips"."lat"))*sin(radians("zips"."long")) + sin(radians(25.95))*sin(radians("zips"."lat"))) * 3982*1.17 AS distance').where('acos(cos(radians(25.95))*cos(radians(-80.14))*cos(radians("zips"."lat"))*cos(radians("zips"."long")) + cos(radians(25.95))*sin(radians(-80.14))*cos(radians("zips"."lat"))*sin(radians("zips"."long")) + sin(radians(25.95))*sin(radians("zips"."lat"))) * 3982*1.17 < 300').joins(:zip)
-# #   
-#   
-#Works, returns all zips and their distance to zip supplied in #where at end
-   Zip.select('zips2.*, acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 AS distance').from('"zips" AS "zips1" CROSS JOIN "zips" AS "zips2"').where('"zips1"."code"=?', '33180').where(distance: 0..100)
-#   
-Zip.find_by_sql(['SELECT zips_with_dist.* FROM (SELECT zips2.*, acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 AS distance FROM "zips" AS "zips1" CROSS JOIN "zips" AS "zips2" WHERE "zips1"."code"=?) AS "zips_with_dist" WHERE "zips_with_dist"."distance" <= ?', '33180', '50')
-#   
-Zip.select("zips_with_dist.*").from('(SELECT zips2.*, acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 AS distance FROM "zips" AS "zips1" CROSS JOIN "zips" AS "zips2" WHERE "zips1"."code"=?) AS "zips_with_dist"').bind('"zips1"."code"' => '33180').where('"zips_with_dist"."distance"<=?').bind('"zips_with_dist"."distance"' => '50')
-#   
-#     Zip.from(Listing.zips_with_distance('33180')).where(['"distance"<? OR "code"=?', '50', '33180')
-    
-      def self.zips_with_distance(zip, radius)
-        Zip.select('zips2.*, acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 AS distance').
-            from('"zips" AS "zips1" CROSS JOIN "zips" AS "zips2"').
-            where(['"zips1"."code"=? OR acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 <= ?', zip, radius])
-      end
-    
-  Listing.select('"listings"."*", "zip"."distance"').joins('INNER JOIN (SELECT zips_with_dist.* FROM (SELECT zips2.*, acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 AS distance FROM "zips" AS "zips1" CROSS JOIN "zips" AS "zips2" WHERE ("zips1"."code"=33180)) AS "zips_with_dist" WHERE "zip"."distance"<50) AS "zip" ON "listings"."zipcode"="zip"."code"')
   
-  
-  def self.search(terms, page)
-    results = Listing.where("listings.model_id IS NOT NULL").
-                      includes(:thumbs, :make, :model, :zip).
-                      page(page)
+ def self.within_miles_from_zip(dist, zip)
+   Listing.select('listings.*, "near_zips"."distance"').
+           joins("INNER JOIN (#{Zip.near(dist, zip).to_sql}) AS \"near_zips\" ON \"near_zips\".\"code\"=\"listings\".\"zipcode\"") 
+ end
+ 
+ def self.search(terms, page)
+   if terms[:zip] && terms[:zip].length == 5 && Zip.find_by_code(terms[:zip])
+     dist = terms[:dist] && terms[:dist].length > 0 ? terms[:dist] : "3500"
+     result = Listing.within_miles_from_zip(dist, terms[:zip])
+   else
+     result = Listing
+   end
+   
+   results = result.where("listings.model_id IS NOT NULL").
+                    includes(:thumbs, :make, :model, :zip).
+                    page(page)
+                    
+    
     if terms
       if terms[:year_from].to_i > 0 && terms[:year_to].to_i > 0
         results = results.where(year: terms[:year_from]..terms[:year_to])
@@ -63,6 +56,11 @@ Zip.select("zips_with_dist.*").from('(SELECT zips2.*, acos(cos(radians("zips1"."
         results = results.where("listings.price <= '#{terms[:price_to]}'")
       end
       
+      results.order_values = []
+      if terms[:sort] == "distance" && (terms[:zip] && terms[:zip].length != 5)
+        terms[:sort] = ""
+      end
+       
       case terms[:sort]
       when "post_date_asc"
         results = results.order(:post_date)
@@ -72,6 +70,8 @@ Zip.select("zips_with_dist.*").from('(SELECT zips2.*, acos(cos(radians("zips1"."
         results = results.order(:price)
       when "price_desc"
         results = results.order("listings.price DESC")
+      when "distance"
+        results = results.order("near_zips.distance ASC")
       else
         results = results.order("listings.id DESC")
       end
--- app/models/zip.rb
+++ app/models/zip.rb
@@ -3,25 +3,56 @@ class Zip < ActiveRecord::Base
   
   has_many :listings, primary_key: :code, foreign_key: :zipcode
   
-  def self.near(zip, radius)
-    Zip.find_by_sql(['SELECT zips_with_dist.* 
-                      FROM (SELECT zips2.*, acos(cos(radians("zips1"."lat"))  *
-                                                 cos(radians("zips1"."long")) *
-                                                 cos(radians("zips2"."lat"))  *
-                                                 cos(radians("zips2"."long")) + 
-                                                 cos(radians("zips1"."lat"))  *
-                                                 sin(radians("zips1"."long")) *
-                                                 cos(radians("zips2"."lat"))  *
-                                                 sin(radians("zips2"."long")) + 
-                                                 sin(radians("zips1"."lat"))  *
-                                                 sin(radians("zips2"."lat"))) 
-                                             * 3982 * 1.17 AS distance 
-                             FROM "zips" AS "zips1" 
-                             CROSS JOIN "zips" AS "zips2" 
-                             WHERE "zips1"."code"=?) 
-                        AS "zips_with_dist" 
-                        WHERE "zips_with_dist"."distance" <= ? 
-                        OR "zips_with_dist"."code"=?', zip, radius, zip])
-  end 
+  
+  def self.near(distance, zip)
+    Zip.select("zips_with_distance.*").
+        from("(#{zips_with_distance_from(zip).to_sql}) AS \"zips_with_distance\"").
+        where(['"zips_with_distance"."distance" <= ? OR "zips_with_distance"."code" = ?', distance, zip]).
+        order('"zips_with_distance"."distance"')
+  end
+
+  def self.zips_with_distance_from(zip)
+    Zip.select('zips2.*, acos(cos(radians("zips1"."lat")) *
+                              cos(radians("zips1"."long"))*
+                              cos(radians("zips2"."lat")) *
+                              cos(radians("zips2"."long"))+ 
+                              cos(radians("zips1"."lat")) *
+                              sin(radians("zips1"."long"))*
+                              cos(radians("zips2"."lat")) *
+                              sin(radians("zips2"."long"))+ 
+                              sin(radians("zips1"."lat")) *
+                              sin(radians("zips2"."lat")))* 
+                         3982 * 1.17 AS distance').
+        from('"zips" AS "zips1" CROSS JOIN "zips" AS "zips2"').
+        where('"zips1"."code"=?', zip)
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  # def self.near(zip, radius)
+  #   Zip.find_by_sql(['SELECT zips_with_dist.* 
+  #                     FROM (SELECT zips2.*, acos(cos(radians("zips1"."lat"))  *
+  #                                                cos(radians("zips1"."long")) *
+  #                                                cos(radians("zips2"."lat"))  *
+  #                                                cos(radians("zips2"."long")) + 
+  #                                                cos(radians("zips1"."lat"))  *
+  #                                                sin(radians("zips1"."long")) *
+  #                                                cos(radians("zips2"."lat"))  *
+  #                                                sin(radians("zips2"."long")) + 
+  #                                                sin(radians("zips1"."lat"))  *
+  #                                                sin(radians("zips2"."lat"))) 
+  #                                            * 3982 * 1.17 AS distance 
+  #                            FROM "zips" AS "zips1" 
+  #                            CROSS JOIN "zips" AS "zips2" 
+  #                            WHERE "zips1"."code"=?) 
+  #                       AS "zips_with_dist" 
+  #                       WHERE "zips_with_dist"."distance" <= ? 
+  #                       OR "zips_with_dist"."code"=?', zip, radius, zip])
+  # end 
   #Zip.select('zips_with_dist.*').from('(SELECT zips2.*, acos(cos(radians("zips1"."lat"))*cos(radians("zips1"."long"))*cos(radians("zips2"."lat"))*cos(radians("zips2"."long")) + cos(radians("zips1"."lat"))*sin(radians("zips1"."long"))*cos(radians("zips2"."lat"))*sin(radians("zips2"."long")) + sin(radians("zips1"."lat"))*sin(radians("zips2"."lat"))) * 3982*1.17 AS distance FROM "zips" AS "zips1" CROSS JOIN "zips" AS "zips2" WHERE "zips1"."code"=?) AS "zips_with_dist"').bind(["code", '33180']).where('"zips_with_dist"."distance" <= ?', '50')
 end
