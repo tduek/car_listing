ed4e78e0450aa5579bef398a56247c60341c5829
Tommy Duek <taduek@gmail.com>
2013-09-18 10:13:02 -0400

    Add Subdivision#average_price.

--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -1,9 +1,16 @@
 class Subdivision < ActiveRecord::Base
   attr_accessible :name, :parent_id, :level
-  
+
   has_many :children, class_name: "Subdivision", foreign_key: :parent_id
-  
+
   belongs_to :parent, class_name: "Subdivision", foreign_key: :parent_id
-  
+
   has_many :spellings
+
+  def average_price(args)
+    result = Listing.where("listings.make_id=? OR listings.model_id=?", id, id)
+    result = result.where(year: args[:year]) if args[:year]
+    result.average("listings.price").to_i
+
+  end
 end
