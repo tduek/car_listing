6edc89e39185826cdf5fe0d7cc116441da1a9e25
Tommy Duek <taduek@gmail.com>
2013-09-27 22:55:52 -0400

    Improve stats queries.

--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -19,11 +19,17 @@ class Subdivision < ActiveRecord::Base
     Subdivision.all_with_stats.find_by_id(self.id)
   end
 
-  def self.all_with_stats
-    Subdivision.select("subdivisions.*, avg(listings.price) AS avg, stddev_pop(listings.price) AS std_dev")
-               .joins("INNER JOIN listings ON listings.model_id=subdivisions.id OR listings.make_id=subdivisions.id")
+  def self.all_with_stats(parent_id = nil)
+    result = Subdivision.select("subdivisions.*,
+                        avg(listings.price) AS avg,
+                        stddev_pop(listings.price) AS std_dev")
+               .joins("INNER JOIN listings
+                               ON listings.model_id=subdivisions.id
+                               OR listings.make_id=subdivisions.id")
                .group("subdivisions.id")
                .having("count(listings.id) > 30")
+
+    parent_id ? result.where(parent_id: parent_id) : result
   end
 
 end
