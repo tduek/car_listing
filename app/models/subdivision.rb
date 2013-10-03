b76fc01d2bea5b0de5d098af1fb5aaa8f6ea4105
Tommy Duek <taduek@gmail.com>
2013-10-03 15:35:18 -0400

    Cleanup Subdivision stats methods.

--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -11,8 +11,7 @@ class Subdivision < ActiveRecord::Base
   def listings(years = nil)
     result = Listing.where("listings.make_id=? OR listings.model_id=?", self.id, self.id)
 
-    result = result.where(year: years) if years
-    result
+    years ? result.where(year: years) : result
   end
 
   def with_stats
@@ -21,13 +20,13 @@ class Subdivision < ActiveRecord::Base
 
   def self.all_with_stats(parent_id = nil)
     result = Subdivision.select("subdivisions.*,
-                        avg(listings.price) AS avg,
-                        stddev_pop(listings.price) AS std_dev")
-               .joins("INNER JOIN listings
-                               ON listings.model_id=subdivisions.id
-                               OR listings.make_id=subdivisions.id")
-               .group("subdivisions.id")
-               .having("count(listings.id) > 30")
+                                 avg(listings.price) AS avg,
+                                 stddev_pop(listings.price) AS std_dev")
+                        .joins("INNER JOIN listings
+                                        ON listings.model_id=subdivisions.id
+                                        OR listings.make_id=subdivisions.id")
+                        .group("subdivisions.id")
+                        .having("count(listings.id) > 30")
 
     parent_id ? result.where(parent_id: parent_id) : result
   end
