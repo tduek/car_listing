b7ac8902cd199e03f8ebc048b4f4fc401206eaed
Tommy Duek <taduek@gmail.com>
2013-10-08 17:33:25 -0400

    Prettier listingscontroller.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,25 +1,25 @@
 class ListingsController < ApplicationController
   def index
-    params[:search] = {} unless params[:search]
+    params[:search] ||= {}
     params[:page] ||= 1
     @makes_array = Subdivision.where(level: 0).order(:name).
                                all.map { |make| [make.name, make.id] }
     @models_array = Subdivision.where(level: 1).order(:name).
                                 all.map { |model| [model.name, model.id] }
-    
+
     @listings = Listing.search(params[:search], params[:page])
-    
-    @sort_options = [["oldest first", "post_date_asc"], 
+
+    @sort_options = [["oldest first", "post_date_asc"],
                      ["newest first", "post_date_desc"],
-                     ["lowest price", "price_asc"], 
+                     ["lowest price", "price_asc"],
                      ["highest price", "price_desc"],
                      ["distance", "distance"]]
-                     
+
     zip = params[:search][:zip]
     if zip && zip.length > 1 && !Zip.find_by_code(zip)
       flash[:alert] = "Invalid zipcode. Please check your input."
     end
-    
+
     if request.xhr?
       #sleep 1 if Rails.env.development?
       render @listings
