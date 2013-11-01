2ae6f966e5c2c43b0b8b1f10a6c2bbdf16d9d066
Tommy Duek <taduek@gmail.com>
2013-11-01 16:12:53 -0400

    Modify pic url in image_tag in _listing.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,23 +1,23 @@
 class Listing < ActiveRecord::Base
-  attr_accessible :is_owner, :miles, :model_id, :phone, :price, :scraping_id, :year, :zipcode, :post_date
-  
+  attr_accessible :is_owner, :miles, :model_id, :phone, :price, :scraping_id, :year, :zipcode, :post_date, :make_id, :title, :description
+
   belongs_to :scraping
   belongs_to :zip, primary_key: :code, foreign_key: :zipcode
-  
+
   belongs_to :make, class_name: "Subdivision", foreign_key: :make_id
   belongs_to :model, class_name: "Subdivision", foreign_key: :model_id
-  
-  has_many :thumbs, class_name: "Pic", 
+
+  has_many :thumbs, class_name: "Pic",
                     conditions: "pics.thumb_for IS NOT NULL"
   has_many :main_pics, class_name: "Pic",
                        conditions: "pics.thumb_for IS NULL"
 
-  
+
  def self.within_miles_from_zip(dist, zip)
    Listing.select('listings.*, "near_zips"."distance"').
-           joins("INNER JOIN (#{Zip.near(dist, zip).to_sql}) AS \"near_zips\" ON \"near_zips\".\"code\"=\"listings\".\"zipcode\"") 
+           joins("INNER JOIN (#{Zip.near(dist, zip).to_sql}) AS \"near_zips\" ON \"near_zips\".\"code\"=\"listings\".\"zipcode\"")
  end
- 
+
  def self.search(terms, page)
    if terms[:zip] && terms[:zip].length == 5 && Zip.find_by_code(terms[:zip])
      dist = terms[:dist] && terms[:dist].length > 0 ? terms[:dist] : "3500"
@@ -25,12 +25,12 @@ class Listing < ActiveRecord::Base
    else
      result = Listing
    end
-   
+
    results = result.where("listings.model_id IS NOT NULL").
                     includes(:thumbs, :make, :model, :zip).
                     page(page)
-                    
-    
+
+
     if terms
       if terms[:year_from].to_i > 0 && terms[:year_to].to_i > 0
         results = results.where(year: terms[:year_from]..terms[:year_to])
@@ -39,15 +39,15 @@ class Listing < ActiveRecord::Base
       elsif terms[:year_to].to_i > 0
         results = results.where("listings.year <= '#{terms[:year_to]}'")
       end
-      
+
       if terms[:make_id].to_i > 0
         results = results.where(make_id: terms[:make_id])
       end
-      
+
       if terms[:model_id].to_i > 0
         results = results.where(model_id: terms[:model_id])
       end
-      
+
       if terms[:price_from].to_i > 0 && terms[:price_to].to_i > 0
         results = results.where(price: terms[:price_from]..terms[:price_to])
       elsif terms[:price_from].to_i > 0
@@ -55,12 +55,12 @@ class Listing < ActiveRecord::Base
       elsif terms[:price_to].to_i > 0
         results = results.where("listings.price <= '#{terms[:price_to]}'")
       end
-      
+
       results.order_values = []
       if terms[:sort] == "distance" && (terms[:zip] && terms[:zip].length != 5)
         terms[:sort] = ""
       end
-       
+
       case terms[:sort]
       when "post_date_asc"
         results = results.order(:post_date)
@@ -76,7 +76,7 @@ class Listing < ActiveRecord::Base
         results = results.order("listings.id DESC")
       end
     end
-    
+
     results.order("listings.post_date DESC")
   end
 end
