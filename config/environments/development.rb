ea69a47faa83f171fc9bb4203ffb9fdf2c0037bc
Tommy Duek <taduek@gmail.com>
2014-01-15 19:59:32 -0500

    Change dev actionmailer url to port 7777.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,5 +45,5 @@ CarListing::Application.configure do
   }
 
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = {host: 'localhost:7777'}
 end
