8c05c44960a40854d78508b6f08d6feea6d7874c
Tommy Duek <taduek@gmail.com>
2014-01-21 11:24:43 -0500

    Get rid of imagemagick styles.

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,7 @@
 class Pic < ActiveRecord::Base
   attr_accessible :listing_id, :thumb_for, :is_thumb, :file
 
-  has_attached_file :file, styles: {sthumb: "50x50!", lthumb: "100x100!", full_size: "650"}
+  has_attached_file :file
 
   belongs_to :listing
 end
