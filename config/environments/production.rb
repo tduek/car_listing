44aaf24368cca2f38726238085f0e59263f02242
Tommy Duek <taduek@gmail.com>
2014-01-30 19:32:55 -0500

    Use Sendgrid for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,14 @@ CarListing::Application.configure do
   }
 
   config.action_mailer.default_url_options = {host: 'tommyscars.com'}
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
