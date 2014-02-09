e370aca7908f59ff674c5d4830dcf80bd7b493c1
Tommy Duek <taduek@gmail.com>
2014-02-09 12:38:43 -0500

    Security. Thanks Gaurav.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CarListing::Application.config.secret_token = '3c6eb8ca96112e13bf43ac806795820e5ec779deb746aa0b1de2f60913d4d2dfbde1273cb8dc5b61a33e7b4af7fc199cc1ebd82d3d47b3ea53eb874bd5444c75'
+CarListing::Application.config.secret_token = ENV["APP_SECRET_TOKEN"]
